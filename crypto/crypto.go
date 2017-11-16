7c7df43a0e1dbd8ae82dcfd9ede69a2812fd93b4
Eftakhairul Islam <eftakhairul@gmail.com>
2017-11-16 00:46:03 -0500

    coinmaketcap API is updated

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -1,27 +1,15 @@
 package crypto
 
-
-import "errors"
-
 //Crypto interface
 type Crypto interface {
 	GetCoinData() []Coin
 	GetCoinDataByCurrency() []Coin
 }
 
-
 //Coin struct
 type Coin struct {
-	Name   string
-	Symbol string
-	Price  float64
+	Name     string
+	Symbol   string
+	Price    float64
 	Currency string
 }
-
-
-type CryptoApi struct {
-	cryptoApi Crypto
-}
-
-
-

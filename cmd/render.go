60c3ff2e60bd8dcd0f9833b41f9846177f291175
Eftakhairul Islam <eftakhairul@gmail.com>
2017-11-25 12:31:55 -0500

    command line render is updated

--- cmd/render.go
+++ cmd/render.go
@@ -1,22 +1,26 @@
 package cmd
 
-
 import (
-	"os"
 	"fmt"
+	"os"
+
 	"github.com/eftakhairul/crypto-terminal-watch/crypto"
 	"github.com/olekukonko/tablewriter"
-	)
+)
 
-// 	Render prints all coins in a nice tabular format in terminal
+// 	Render all coins in a nice tabular format in terminal
 func Render(coins []crypto.Coin) {
 
-	data :=  make( [][]string, len(coins))
+	if len(coins) == 0 {
+		fmt.Println("Sorry! Please try agian. To get info about BTC in CAD: ctw --currency CAD BTC")
+		return
+	}
+
+	data := make([][]string, len(coins))
 
 	for i := 0; i < len(coins); i++ {
 		data[i] = []string{coins[i].Name, coins[i].Symbol, fmt.Sprintf("%.10f %s", coins[i].Price, coins[i].Currency)}
 	}
-
 
 	// Final Printing into console
 	table := tablewriter.NewWriter(os.Stdout)

fd519a5f06b3cb3aae2e7fa1869f10c3fb63a2a7
Eftakhairul Islam <eftakhairul@gmail.com>
2022-01-22 23:09:38 -0500

    feat(homebrew): homebrew support is updated

--- ctw.rb
+++ ctw.rb
@@ -3,9 +3,7 @@ class Ctw < Formula
   homepage "https://github.com/eftakhairul/crypto-terminal-watch"
   url "http://apps.eftakhairul.com/ctw.tar.gz"
   version "0.0.1"
-  sha256 "4f6376cfcb4c8ea464aa39f3808c7ee3bb0bf23d301ea369582a1fd6809105f1"  
-
-  bottle :unneeded
+  sha256 "4f6376cfcb4c8ea464aa39f3808c7ee3bb0bf23d301ea369582a1fd6809105f1"
 
   def install
     bin.install "ctw"
